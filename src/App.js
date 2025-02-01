586ab3900bd3810d841978469cefa76720fcaa36
cetsandeepwork <159258159+cetsandeepwork@users.noreply.github.com>
2025-02-01 13:24:42 +0530

    new add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-import PdfEditor from './components/PdfEditor';
-import Test from './components/Test';
 import ProductPage from './pages/DetailPage';
-import BarcodeScanner from './pages/Scanner';
-import Tracking from './pages/Tracking';
-import { pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
-// Set the workerSrc to load the PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
   return (
     <div className="App">
      <ProductPage/>
-     {/* <Test/> */}
     </div>
   );
 }
